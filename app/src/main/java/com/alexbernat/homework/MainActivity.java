aa1afba08e22d171c353e3767337e15e599b0fd5
Alexander <bernatik@tut.by>
2017-07-25 20:16:38 +0300

    Task 1

--- app/src/main/java/com/alexbernat/homework/MainActivity.java
+++ app/src/main/java/com/alexbernat/homework/MainActivity.java
@@ -1,13 +1,53 @@
 package com.alexbernat.homework;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView tvUp, tvDown;
+    private Button btnChange;
+    private View.OnClickListener varListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+             changeText();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvUp = (TextView)findViewById(R.id.text_up);
+        tvDown = (TextView)findViewById(R.id.text_down);
+        btnChange = (Button)findViewById(R.id.button_change);
+
+        /* click as a variable */
+        tvUp.setOnClickListener(varListener);
+        /* click as an interface implemented by activity */
+        tvDown.setOnClickListener(this);
+        /* click as an anonymous class */
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeText();
+            }
+        });
+    }
+
+    private void changeText(){
+        String tempUp = String.valueOf(tvUp.getText());
+        String tempDown = String.valueOf(tvDown.getText());
+        tvUp.setText(tempDown);
+        tvDown.setText(tempUp);
+    }
+
+    @Override
+    public void onClick(View v) {
+        changeText();
     }
 }
