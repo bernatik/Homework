c4041458eefeaa6924602720a7e0fd2bd0cdeb64
Alexander <bernatik@tut.by>
2017-07-28 23:57:17 +0300

    Change button "show image" in Homework 3

--- app/src/main/java/com/alexbernat/homework3/Homework3Activity.java
+++ app/src/main/java/com/alexbernat/homework3/Homework3Activity.java
@@ -1,10 +1,20 @@
 package com.alexbernat.homework3;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.alexbernat.homework.R;
 
 /**
  * Created by Александр on 28.07.2017.
  */
 
 public class Homework3Activity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework3);
+    }
 }
