6771bbed2b9ed8e0a2e4afdb8afccba9d977c560
Alexander <bernatik@tut.by>
2017-08-02 23:51:28 +0300

    Add Homework 4: Part 1 - transition animation (from activity to activity)

--- app/src/main/java/com/alexbernat/homework4/Homework4Activity.java
+++ app/src/main/java/com/alexbernat/homework4/Homework4Activity.java
@@ -1,7 +1,10 @@
 package com.alexbernat.homework4;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.alexbernat.homework.R;
 
@@ -11,5 +14,14 @@ public class Homework4Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homework4);
+
+        Button btnNewActivity = (Button)findViewById(R.id.button_homework4_start_new_activity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Homework4Activity.this, Homework4TempActivity.class));
+                overridePendingTransition(R.anim.appearance, R.anim.disappearance);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/alexbernat/homework4/Homework4TempActivity.java
@@ -0,0 +1,15 @@
+package com.alexbernat.homework4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.alexbernat.homework.R;
+
+public class Homework4TempActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework4_temp);
+    }
+}
