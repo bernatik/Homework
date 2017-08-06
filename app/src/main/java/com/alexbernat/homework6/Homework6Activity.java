cd10f9ad793b176f8601ba696ed57cba7bd6105e
Alexander <bernatik@tut.by>
2017-08-06 14:46:04 +0300

    Add homework6: Gallery loaded with Glide and Progress Bar

--- app/src/main/java/com/alexbernat/homework6/Homework6Activity.java
+++ app/src/main/java/com/alexbernat/homework6/Homework6Activity.java
@@ -2,14 +2,41 @@ package com.alexbernat.homework6;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.alexbernat.homework.R;
 
+import java.util.ArrayList;
+
 public class Homework6Activity extends Activity {
+
+    RecyclerView rvGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homework6);
+
+        rvGallery = (RecyclerView)findViewById(R.id.rv_homework6);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        rvGallery.setLayoutManager(layoutManager);
+
+        ArrayList<String> pictureUrls = new ArrayList<>();
+        pictureUrls.add("https://s-media-cache-ak0.pinimg.com/236x/ee/cf/37/eecf37527700cce34dc313737203ccc2--comedy-movies-hd-movies.jpg");
+        pictureUrls.add("https://s-media-cache-ak0.pinimg.com/originals/b0/75/68/b07568db6c706ac6f0bb63c5dee21d5d.jpg");
+        pictureUrls.add("https://s-media-cache-ak0.pinimg.com/236x/5b/bc/09/5bbc0921b795fb5a24a32404caf040a8--film-poster-movie-posters.jpg");
+        pictureUrls.add("https://s-media-cache-ak0.pinimg.com/236x/3e/2a/2b/3e2a2b4f7eadd7d1ec403373438c549e--hd-movies-movies-free.jpg");
+        pictureUrls.add("https://s-media-cache-ak0.pinimg.com/236x/27/09/2b/27092bbb5f20a43c7e04e46429de4964.jpg");
+        pictureUrls.add("https://www.movie2free.com/wp-content/uploads/2015/05/SmallSoldiers1.jpg");
+        pictureUrls.add("https://s-media-cache-ak0.pinimg.com/originals/57/c4/8b/57c48b8dfde29e078ed46ed8a3dddadb.jpg");
+        pictureUrls.add("https://s-media-cache-ak0.pinimg.com/236x/b5/11/6c/b5116cf85b309b39f201d780c1a235a7--ip-man-man-.jpg");
+        pictureUrls.add("https://s-media-cache-ak0.pinimg.com/236x/13/81/30/138130c7ff0ee9da2a26e88935f49476--watch-free-movies-movies-free.jpg");
+        pictureUrls.add("https://i.pinimg.com/236x/28/94/1f/28941fd4d1e50ba307b12cfdbce192bc--kung-fu-movies--movie.jpg");
+
+        Homework6Adapter adapter = new Homework6Adapter(pictureUrls, this);
+        rvGallery.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/com/alexbernat/homework6/Homework6Adapter.java
@@ -0,0 +1,79 @@
+package com.alexbernat.homework6;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.alexbernat.homework.R;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Александр on 06.08.2017.
+ */
+public class Homework6Adapter extends RecyclerView.Adapter<Homework6Adapter.Homework6ViewHolder> {
+
+    ArrayList<String> urls;
+    Context context;
+
+    public Homework6Adapter(ArrayList<String> urls, Context context) {
+        this.urls = urls;
+        this.context = context;
+    }
+
+    @Override
+    public Homework6ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View rootView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_homework6, parent, false);
+        return new Homework6ViewHolder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(final Homework6ViewHolder holder, int position) {
+        Glide.with(context)
+                .load(urls.get(position))
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        holder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        holder.progressBar.setVisibility(View.GONE);
+                        holder.image.setVisibility(View.VISIBLE);
+                        return false;
+                    }
+                })
+                .into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return urls == null ? 0 : urls.size();
+    }
+
+    public class Homework6ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        ProgressBar progressBar;
+
+        public Homework6ViewHolder(View itemView) {
+            super(itemView);
+            image = (ImageView)itemView.findViewById(R.id.image_homework6);
+            progressBar = (ProgressBar)itemView.findViewById(R.id.progress_bar_homework6);
+        }
+    }
+}
