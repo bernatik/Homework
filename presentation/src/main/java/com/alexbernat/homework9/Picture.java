73bbaa0dcf35ee9c98a7c1844981b77aed145d6e
Alexander <bernatik@tut.by>
2017-08-13 18:44:10 +0300

    Bind model with xml_item

---
+++ presentation/src/main/java/com/alexbernat/homework9/Picture.java
@@ -0,0 +1,12 @@
+package com.alexbernat.homework9;
+
+/**
+ * Created by Александр on 13.08.2017.
+ */
+public class Picture {
+    public String linkToPicture;
+
+    public Picture(String linkToPicture) {
+        this.linkToPicture = linkToPicture;
+    }
+}
